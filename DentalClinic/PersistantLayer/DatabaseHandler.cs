3b43693bbf27761e5d744e4f18bd2d58400004e2
MitFarrelly <mitfarrelly@hotmail.com>
2014-04-29 12:54:49 +1000

    project start

---
+++ DentalClinic/PersistantLayer/DatabaseHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Data;
+
+namespace PersistantLayer
+{
+    public class DatabaseHandler
+    {
+        OleDbConnection oleConn;
+        string connString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data source=  "; //add source
+
+        public DatabaseHandler()
+        {
+            oleConn = new OleDbConnection(connString);
+            try
+            {
+                oleConn.Open();
+                oleConn.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
